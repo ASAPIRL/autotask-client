3239bed81947ac667ddac361ea761de04cd59eb2
w911 <tema@asapirl.com>
2023-05-17 11:08:08 +0400

    fix

--- src/API/ArticleTagAssociations/ArticleTagAssociationQueryBuilder.php
+++ src/API/ArticleTagAssociations/ArticleTagAssociationQueryBuilder.php
@@ -56,10 +56,6 @@ class ArticleTagAssociationQueryBuilder
             ]);
         }
 
-         $response = $this->client->get("ArticleTagAssociations/query/count", [
-             'search' => json_encode( $this->toArray() )
-         ]);
-
          $responseArray = json_decode($response->getBody(), true);
 
          if (! isset($responseArray['queryCount'])) {
